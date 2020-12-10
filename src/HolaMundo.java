204fe2cc4a2013554305914c7de70fac3fa55c19
santima1904 <santima1904@gmail.com>
2020-12-10 14:08:02 +0100

    commit prueba

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 //proyecto  para el ciclo
 	public static void main(String[] args) {
 
-		System.out.println("Martinez Aral");
+		System.out.println("Martinez  Aral");
 
 	}
 

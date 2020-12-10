db0f60e99f84fbd457fe3034863c688c237cb06d
santima1904 <santima1904@gmail.com>
2020-12-10 13:43:18 +0100

    sin falta de ortografía

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,12 +1,9 @@
 
 public class HolaMundo {
-//prollecto  para el ciclo
+//proyecto  para el ciclo
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Martinez Aral");
-=======
-		System.out.println("Santiago");
->>>>>>> branch 'master' of https://github.com/santima1904/ProyectoSantiagoMartinezExamen.git
 
 	}
 
